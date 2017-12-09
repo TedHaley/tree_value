cb4b95eae1aa9a00f42c571dfc99bbd7c0177b43
Edward Haley <teddyhaley1@gmail.com>
2017-12-09 00:18:20 -0800

    Working map output from shapefile with colour coding

--- src/viz_data.R
+++ src/viz_data.R
@@ -2,37 +2,68 @@
 # viz_data.R
 # Ted Haley Dec, 2017
 
-# This script takes the cleaned data from the results folder and performs various analyses on the data. 
-# The goal of this script is to test the hypothesis that the size, age, and number of trees has a direct 
-# effect on the value of land in the City of Vancouver.
+# This script takes the cleaned data and the summarized data from the results folder and makes visualizatons from the data.
+# The results from the analysis will be displayed using various graphs, plots, and maps.
 
 # Usage: Rscript src/viz_data.R
 
 suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(ggmap))
-suppressPackageStartupMessages(library(sf))
-suppressPackageStartupMessages(library(raster))
-suppressPackageStartupMessages(library(maptools))
 suppressPackageStartupMessages(library(ggplot2))
 suppressPackageStartupMessages(library(rgdal))
 
+# Import clean data
 tree_data <- read.csv(file="results/tree_data_final.csv", header=TRUE, sep=",")
+tax_data <- read.csv(file="results/tax_data_final.csv", header=TRUE, sep=",")
+shp_areas <- read.csv(file="results/shp_areas_final.csv", header=TRUE, sep=",")
 
+# Import summarized data from analysis
+sumr_tree_size_neigh <- read.csv(file="results/sumr_tree_size_neigh.csv", header=TRUE, sep=",")
+sumr_tree_size_type <- read.csv(file="results/sumr_tree_size_type.csv", header=TRUE, sep=",")
+sumr_tree_type_neigh <- read.csv(file="results/sumr_tree_type_neigh.csv", header=TRUE, sep=",")
+sumr_most_common <- read.csv(file="results/sumr_most_common.csv", header=TRUE, sep=",")
+sumr_neigh_yr_planted <- read.csv(file="results/sumr_neigh_yr_planted.csv", header=TRUE, sep=",")
+sumr_land_val_neigh <- read.csv(file="results/sumr_land_val_neigh.csv", header=TRUE, sep=",")
+
+# Import Vancouver shapefile
 van_shp <- readOGR(dsn = "data/local_area_boundary_shp/", layer = "local_area_boundary") %>% 
   spTransform(CRS("+proj=longlat +datum=WGS84"))
-#overlay <- fortify(overlay, region="COMMUNITY")
 
-location <- unlist(geocode('357 W King Edward Ave, Vancouver, BC V5Y 2J1'))+c(-0.01,-0.005)
+# Makes shape file into a dataframe
+van_shp_fort <- van_shp %>% 
+  fortify(region = 'MAPID')
 
-gmap <- get_map(location=location, zoom = 12, maptype = "terrain", source = "google", col="bw")
+names(shp_areas) <- c("id", "NEIGHBOURHOOD_NAME", "NEIGHBOURHOOD_CODE")
 
-van_shp_map <- ggmap(gmap) +
-  geom_polygon(data=van_shp, aes(x=long, y=lat, group=group), color="red", alpha=0) +
-  coord_map() +
-  theme_bw()
+#This joins the shape file data and the neighbourhood code identifier: This is the KEY between datasets
+key <- left_join(van_shp_fort, shp_areas)
 
-van_shp_map
+# tree_size_neigh on the map
+tree_size_neigh_map <- left_join(key, sumr_tree_size_neigh)
 
+g <- ggplot(tree_size_neigh_map, aes(long, lat, group = group)) + 
+  geom_polygon(aes(fill = mean_dia))
+
+g
+# 
+# van_hoods  = merge(van_shp_fort, van_shp@data, by.x = 'id', by.y = 'ZIP_CODE')
+#sf = merge(SFNeighbourhoods, values, by.x='id')
+#sf %>% group_by(id) %>% do(head(., 1)) %>% head(10)
+
+# 
+# names(van_shp)
+# head(van_shp)
+
+# location <- unlist(geocode('357 W King Edward Ave, Vancouver, BC V5Y 2J1'))+c(-0.01,-0.005)
+# 
+# gmap <- get_map(location=location, zoom = 12, maptype = "terrain", source = "google", col="bw")
+# 
+# van_shp_map <- ggmap(gmap) +
+#   geom_polygon(data=van_shp, aes(x=long, y=lat, group=group), color="red", alpha=0) +
+#   coord_map() +
+#   theme_bw()
+# 
+# van_shp_map
 
 
 
